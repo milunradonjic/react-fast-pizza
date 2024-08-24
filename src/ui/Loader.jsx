e15a5d0c1c00211dcfc087112b5ae8a6789e0328
milunradonjic <milun.radonjic95@gmail.com>
2024-08-24 18:53:19 +0200

    position and style loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="abasolute inset-0/20 back-blur-sm flex items-center justify-center bg-slate-200">
+      <div className="loader"></div>
+    </div>
+  );
 }

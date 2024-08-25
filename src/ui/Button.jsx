d85a07f7eb1a6cb18306260daba7b92de2fd2ae7
milunradonjic <milun.radonjic95@gmail.com>
2024-08-25 19:21:17 +0200

    style the menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,19 +1,24 @@
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-6";
+export default function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 sm:px-6 sm:py-6",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to) {
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
